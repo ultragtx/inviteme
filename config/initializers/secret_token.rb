cdf17327bc78e9a99cb0a575d81bba144457e99b
ultragtx <ultragtx@gmail.com>
2013-04-28 15:18:01 +0800

    Bootstrappers init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Inviteme::Application.config.secret_token = '4cb463702947367419c5c062c5f4a07612b2502d368c0919eef9b8d0046d1ef33d6e6c8e5794ccd7851571d967e6ffac639899cedefd39aab78df10666cc798f'
---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Inviteme::Application.routes.draw do
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+
+  root :to => 'high_voltage/pages#show', :id => 'welcome'
+
+end
